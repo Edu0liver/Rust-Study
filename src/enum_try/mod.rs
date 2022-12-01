7824d478faea75ab7da9289d00831680e9e4685f
Eduardo Oliveira Alves <eduardo.alves@icc.org.br>
2022-12-01 15:24:46 -0300

    enum_try

--- src/enum_try/mod.rs
+++ src/enum_try/mod.rs
@@ -1,11 +1,15 @@
+use std::str;
+use std::str::FromStr;
 
 mod enum_try {
     
+    #[derive(Debug)]
     enum IpAddrKind {
-        V4(u8, u8, u8, u8),
+        V4((u8, u8, u8, u8)),
         V6(String)
     }
     
+    #[derive(Debug)]
     struct IpAddr {
         kind: IpAddrKind,
         address: String
@@ -19,14 +23,42 @@ mod enum_try {
     }
 
     impl Message {
-        fn some_function() {
-            println!("Olá");
+        fn some_function() -> String {
+            String::from("Olá :)")
         }
     }
     
+    impl IpAddr {
+        fn new(address: String) -> IpAddr {
+            let address_tuple = ip_address_string_to_tuple(&address);
+
+            match address_tuple {
+                IpAddrKind::V4(_) => 
+                    return IpAddr {
+                        kind: address_tuple,
+                        address
+                    },
+                _ => 
+                    return IpAddr {
+                        kind: IpAddrKind::V6(address.clone()),
+                        address
+                    }
+            }
+
+        }
+    }
+
+    fn ip_address_string_to_tuple(address: &String) -> IpAddrKind {
+        let address_vec: Vec<u8> = address.split(".").map(|x| x.parse::<u8>().unwrap()).collect();
+        let address_tuple: (u8, u8, u8, u8) = (address_vec[0], address_vec[1], address_vec[2], address_vec[3]);
+
+        IpAddrKind::V4(address_tuple)
+    }
+    
     pub fn enum_call() {
-        let message = Message::some_function();
-        println!("{:#?}", message);
+        let ip_address = IpAddr::new(String::from("127.0.0.1"));
+
+        println!("{:#?}", ip_address);
     }
 
 }
